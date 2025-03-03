43032804cd4c70a5c109b6a5b854c89a63af754b
Shaula Paz Garay <a01712013tec.mx>
2025-03-03 13:19:25 -0600

    Lab 10

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Middleware
+app.use((request, response, next) => {
+  console.log("Middleware!");
+
+  //Le permite a la petición avanzar hacia el siguiente middleware
+  next();
+});
+
+const plantasRoutes = require("./routes/plantas.routes");
+
+app.use("/plantas", plantasRoutes);
+
+app.use((request, response, next) => {
+  console.log("Otro middleware!");
+
+  //Manda la respuesta
+  response.send("¡Hola mundo!");
+});
+
+app.listen(3000);
