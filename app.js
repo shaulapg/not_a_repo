62c6356b15e8dbb0a799f0b1b629edbf079c648f
Shaula Paz Garay <a01712013tec.mx>
2025-03-04 13:23:16 -0600

    Lab11 corregido

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const bodyParser = require("body-parser");
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
-  console.log("Middleware!");
+    console.log('Middleware!');
 
-  //Le permite a la petición avanzar hacia el siguiente middleware
-  next();
+    //Le permite a la petición avanzar hacia el siguiente middleware
+    next(); 
 });
 
-const plantasRoutes = require("./routes/plantas.routes");
+const plantasRoutes = require('./routes/plantas.routes');
 
-app.use("/plantas", plantasRoutes);
+app.use('/plantas', plantasRoutes);
 
 app.use((request, response, next) => {
-  console.log("Otro middleware!");
-
-  //Manda la respuesta
-  response.send("¡Hola mundo!");
+    console.log('Otro middleware!');
+    
+    //Manda la respuesta
+    response.statusCode = 404;
+    response.send('No se encuentra el recurso que estás buscando'); 
 });
 
 app.listen(3000);
