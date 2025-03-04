292ee22a14eb94b13bb330465ba0534da5e7a97b
Shaula Paz Garay <a01712013tec.mx>
2025-03-04 13:59:19 -0600

    Archivos de Lab 11 sin Lab 12

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const app = express();
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,8 +21,7 @@ app.use((request, response, next) => {
     console.log('Otro middleware!');
     
     //Manda la respuesta
-    response.statusCode = 404;
-    response.send('No se encuentra el recurso que estás buscando'); 
+    response.send('¡Hola mundo!'); 
 });
 
 app.listen(3000);
