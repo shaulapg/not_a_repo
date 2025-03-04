71e6d8006418529d697a6c740160106474753e69
Shaula Paz Garay <a01712013tec.mx>
2025-03-04 14:48:23 -0600

    Crear modulo users

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const session = require('express-session');
+
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-
-    //Le permite a la petición avanzar hacia el siguiente middleware
-    next(); 
-});
+const usersRoutes = require('./routes/users.routes');
+app.use('/users', usersRoutes);
 
 const plantasRoutes = require('./routes/plantas.routes');
-
 app.use('/plantas', plantasRoutes);
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
     
     //Manda la respuesta
-    response.send('¡Hola mundo!'); 
+    response.statusCode = 404;
+    response.send('No se encuentra el recurso que estás buscando'); 
 });
 
 app.listen(3000);
